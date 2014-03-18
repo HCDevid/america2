8d0bbd521a8ae2bb615e2ac6bd3f044d655a3cc3
HCDevid <hcdevid@gmail.com>
2014-03-18 02:33:52 -0400

    Set error messages & all posts views/controller actions except for edits

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true,
+                    length: {minimum: 5}
+  validates :price, presence: true
+  validates :description, presence: true
 end
