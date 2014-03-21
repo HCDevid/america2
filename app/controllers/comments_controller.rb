66444acfd5e5fb457f1e9fa6aa1f42d45f5a52e4
HCDevid <hcdevid@gmail.com>
2014-03-20 21:00:34 -0400

    Finished basics, about to try a test view with Bootstrap

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+http_basic_authenticate_with name: "dhh", password: "secret",
+  only: :destroy
 
   def create
     @post = Post.find(params[:post_id])
@@ -6,6 +8,12 @@ class CommentsController < ApplicationController
       redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 
 
 
